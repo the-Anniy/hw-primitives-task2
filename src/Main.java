b5218b6415b59068edd1235362390d92ca0c8c54
Anna Paliy <annpaliy2015@gmail.com>
2024-10-29 13:58:50 +0300

    code reformated

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    int amount = 2890;
-    int add = 345;
-    int total = amount + add;
-    int bonus = add / 100;
+        int amount = 2890;
+        int add = 345;
+        int total = amount + add;
+        int bonus = add / 100;
 
-      System.out.println("Итоговая сумма на счете: " + total + "рублей");
-      if (add > 1000) {
-          System.out.println("Начислено бонусов: " + bonus);
-    } else {
-          System.out.println("Бонусы не начислены");
-      }
+        System.out.println("Итоговая сумма на счете: " + total + "рублей");
+        if (add > 1000) {
+            System.out.println("Начислено бонусов: " + bonus);
+        } else {
+            System.out.println("Бонусы не начислены");
+        }
 
-  }
+    }
 }
